0aa340e68a2b907d722342b383463c54c0b2ae16
mcaicedo-csumb <mcaicedo@csumb.edu>
2025-07-27 22:51:13 -0700

    Final changes to GymLogRepository to match professor's version

---
+++ app/src/main/java/com/example/gymlog/database/GymLogRepository.java
@@ -0,0 +1,58 @@
+package com.example.gymlog.database;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.example.gymlog.MainActivity;
+import com.example.gymlog.database.entities.GymLog;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class GymLogRepository {
+    private GymLogDAO gymLogDAO;
+    private ArrayList<GymLog> allLogs;
+
+    private static GymLogRepository repository;
+
+    private GymLogRepository(Application application) {
+        GymLogDatabase db = GymLogDatabase.getDatabase(application);
+        this.gymLogDAO = db.gymLogDAO();
+        this.allLogs = (ArrayList<GymLog>) this.gymLogDAO.getAllRecords();
+    }
+
+    public static GymLogRepository getRepository(Application application) {
+        if (repository != null) {
+            return repository;
+        }
+
+        Future<GymLogRepository> future = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<GymLogRepository>() {
+                    @Override
+                    public GymLogRepository call() throws Exception {
+                        return new GymLogRepository(application);
+                    }
+                }
+        );
+
+        try {
+            repository = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.d(MainActivity.TAG, "Problem getting GymLogRepository, thread error.");
+        }
+
+        return repository;
+    }
+
+    public ArrayList<GymLog> getAllLogs() {
+        return allLogs;
+    }
+
+    public void insertGymLog(GymLog gymLog) {
+        GymLogDatabase.databaseWriteExecutor.execute(() -> {
+            gymLogDAO.insert(gymLog);
+        });
+    }
+}
